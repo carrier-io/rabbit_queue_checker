7ae3c10253554f6fa03610a8d4ded46a51eb7bcb
Aspect13 <ad13box@gmail.com>
2023-04-10 18:29:48 +0400

    workflow for zip build

--- lambda.py
+++ lambda.py
@@ -1,30 +1,81 @@
+from os import environ
 import json
 from arbiter import Arbiter
 import requests
-from os import environ
+
+
+def get_vhost_queues(host: str, port: int, user: str, password: str, vhost: str, timeout: int) -> list:
+    arbiter = Arbiter(
+        host=host,
+        port=int(port),
+        user=user,
+        password=password,
+        vhost=vhost,
+        timeout=int(timeout)
+    )
+    try:
+        queues = list(arbiter.workers().keys())
+        arbiter.close()
+    except:
+        queues = []
+    return queues
 
 
 def handler(event=None, context=None):
-    try:
-        user = event[0]["user"] if event else environ.get("rabbit_project_user")
-        password = event[0]["password"] if event else environ.get("rabbit_project_password")
-        vhost = event[0]["vhost"] if event else environ.get("rabbit_project_vhost")
-        arbiter = Arbiter(host=environ.get("rabbit_host"), port=5672, user=user, password=password, vhost=vhost,
-                          timeout=int(environ.get("AWS_LAMBDA_FUNCTION_TIMEOUT", 120)))
+    debug_sleep = environ.get("debug_sleep")
+    if debug_sleep:
+        print('sleeping for', debug_sleep)
         try:
-            queues = list(arbiter.workers().keys())
-            arbiter.close()
-        except:
-            queues = []
+            from time import sleep
+            sleep(int(debug_sleep))
+        except ValueError:
+            ...
 
-        url = f"{environ.get('galloper_url')}/api/v1/projects/rabbitmq/{vhost}"
-        data = {"queues": json.dumps(queues)}
-        headers = {'content-type': 'application/json'}
-        if environ.get("token"):
-            headers['Authorization'] = f'bearer {environ.get("token")}'
-        requests.put(url, json=data, headers=headers)
+    user = environ.get("rabbit_user")
+    password = environ.get("rabbit_password")
+    host = environ.get("rabbit_host")
+    port = environ.get("rabbit_port", 5672)
+    timeout = environ.get("AWS_LAMBDA_FUNCTION_TIMEOUT", 120)
+
+    put_url = environ.get('put_url')
+    project_ids_get_url = environ.get('project_ids_get_url')
+
+    vhost_template = environ.get("vhost_template", 'project_{project_id}_vhost')
+    core_vhost = environ.get("core_vhost", 'carrier')
+
+    headers = {'content-type': 'application/json'}
+    if environ.get("token"):
+        headers['Authorization'] = f'{environ.get("token_type", "bearer")} {environ.get("token")}'
+
+    try:
+        port = int(port)
+        timeout = int(timeout)
+        if debug_sleep:
+            print('getting queues for', core_vhost)
+        all_queues = {
+            core_vhost: get_vhost_queues(host, port, user, password, core_vhost, timeout)
+        }
+        if debug_sleep:
+            print('got queues for', all_queues)
+            print('getting project ids')
+        project_ids = requests.patch(project_ids_get_url, headers=headers).json()
+        if debug_sleep:
+            print('got project ids:', project_ids)
+
+        for i in project_ids:
+            vhost = vhost_template.format(project_id=i)
+            if debug_sleep:
+                print('getting queues for', vhost)
+            queues = get_vhost_queues(host, port, user, password, vhost, timeout)
+            all_queues[vhost] = queues
+            if debug_sleep:
+                print('got queues for', vhost, ' ', queues)
+
+        requests.put(put_url, json=all_queues, headers=headers)
 
     except Exception as e:
+        if debug_sleep:
+            print(e)
         return {
             'statusCode': 500,
             'body': json.dumps(str(e))
